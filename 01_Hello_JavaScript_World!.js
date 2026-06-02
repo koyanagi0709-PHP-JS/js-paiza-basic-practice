2d634196c67127535705734c68e08aa8b6e1a1a0
koyanagi0709 <koyanagi0709@gmail.com>
2026-06-02 16:29:06 +0900

    初めてのJavaScript Paiza Hello JavaScript World!

---
+++ 01_Hello_JavaScript_World!.js
@@ -0,0 +1,10 @@
+console.log("Hello JavaScript World!");
+
+console.log("1行目の出力");
+console.log("2行目の出力");
+console.log();
+console.log("3行目の出力");
+console.log("4行目の出力");
+console.log("5行目の出力");
+
+console.log("こんにちは、\n世界");
