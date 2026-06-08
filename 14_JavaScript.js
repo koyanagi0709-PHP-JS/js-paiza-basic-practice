5b12f0776d1bdde172bc4d1335e64d9d64f5d7e0
koyanagi0709 <koyanagi0709@gmail.com>
2026-06-09 05:47:20 +0900

    for文整理しました。

--- 14_JavaScript.js
+++ 14_JavaScript.js
@@ -1,7 +1,7 @@
-// for (let i = 0; i < 5; i = i + 1){
-//     console.log(i);
-// }
+for (let i = 0; i < 5; i = i + 1){ // i はカウンタ変数と呼ばれている
+    console.log(i);
+}
 
-for(let i =0; i <5; i = i + 1) {
-    console.log(`i:${i}`);
+for(let i=0; i < 8; i++) { // i++ インクリメント演算子 i--デクリメント演算子
+    console.log(i);
 }
